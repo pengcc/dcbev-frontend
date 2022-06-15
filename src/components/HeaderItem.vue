e7ef4f0bdfe7bb2f77b24198514bb28d1823274e
pccui <p.cui@mobizcorp.de>
2022-06-15 14:01:00 +0200

    feat: initial commit

---
+++ src/components/HeaderItem.vue
@@ -0,0 +1,10 @@
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/admin">Admin</RouterLink>
+      </nav>
+    </div>
+  </header>
+</template>
